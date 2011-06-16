686686e3c217ceec1fef02e2e81c1e752544e106
daniel.korzekwa <daniel.korzekwa@851a5dee-4224-aa8f-6f06-4bf9afbe9161>
2011-06-16 20:26:32 +0000

    Issue 1:	Implement Betex Feeder and deploy it to amazon ec2

--- trunk/src/main/scala/dk/betex/feeder/BetexFeederApp.scala
+++ trunk/src/main/scala/dk/betex/feeder/BetexFeederApp.scala
@@ -1,20 +1,70 @@
 package dk.betex.feeder
 
-/**The main class for Betex Feeder application.
- * 
+import dk.bettingai.marketcollector.task._
+import dk.bot.betfairservice._
+import dk.bettingai.marketcollector.marketservice._
+import org.slf4j.LoggerFactory
+
+/**
+ * The main class for Betex Feeder application.
+ *
  * @author korzekwad
  */
-object BetexFeederApp extends App{
+object BetexFeederApp extends App {
+
+  private val log = LoggerFactory.getLogger(getClass)
 
   printHeader()
-  
-  println("Starting Betex Feeder...");
-  
-  println("Betex Feeder started\nHit enter to stop it...");
-  System.in.read();
-  System.exit(0);
-  
-    private def printHeader() {
+
+  /**Parse input data.*/
+  try {
+    val inputData: Map[String, String] = UserInputParser.parse(args)
+    require(
+      inputData.contains("bfUser") &&
+        inputData.contains("bfPassword") &&
+        inputData.contains("bfProductId") &&
+        inputData.contains("collectionInterval") &&
+        inputData.contains("discoveryInterval") &&
+        inputData.contains("startInMinutesFrom") &&
+        inputData.contains("startInMinutesTo") &&
+        inputData.contains("betexUrl"), "All input arguments must be provided.")
+
+    println("Starting Betex Feeder...");
+
+    /**Create betfair service and login to betfair account.*/
+    val betfairServiceFactoryBean = new dk.bot.betfairservice.DefaultBetFairServiceFactoryBean();
+    betfairServiceFactoryBean.setUser(inputData("bfUser"))
+    betfairServiceFactoryBean.setPassword(inputData("bfPassword"))
+    betfairServiceFactoryBean.setProductId(inputData("bfProductId").toInt)
+    val loginResponse = betfairServiceFactoryBean.login
+    val betfairService: BetFairService = (betfairServiceFactoryBean.getObject.asInstanceOf[BetFairService])
+
+    /**Create event collector task.*/
+    val marketService = new MarketService(betfairService)
+    val maxNumberOfWinners = if (inputData.contains("maxNumOfWinners")) Option(inputData("maxNumOfWinners").toInt) else None
+    val betexAdaptor = BetexAdaptor(inputData("betexUrl"))
+    val marketEventListener = MarketEventListener(betexAdaptor)
+    val eventCollectorTask = new EventCollectorTask(marketService, inputData("startInMinutesFrom").toInt, inputData("startInMinutesTo").toInt, maxNumberOfWinners, inputData("discoveryInterval").toInt, marketEventListener)
+
+    val collectionInterval = inputData("collectionInterval").toLong
+
+    println("Betex Feeder started.");
+
+    while (true) {
+      log.info("Betex Feeder is running.")
+      try {
+        eventCollectorTask.execute()
+      } catch {
+        case e: BetFairException => log.error("Betex Feeder error.", e)
+      }
+      Thread.sleep(collectionInterval)
+    }
+
+  } catch {
+    case e: Exception => printHelpMessage(); println("\n" + e);
+  }
+
+  private def printHeader() {
     println("")
     println("***********************************************************************************")
     println("*Betting Exchange Feeder Copyright 2011 Daniel Korzekwa(http://danmachine.com)    *")
@@ -22,5 +72,16 @@ object BetexFeederApp extends App{
     println("*Licenced under Apache License 2.0(http://www.apache.org/licenses/LICENSE-2.0)    *")
     println("***********************************************************************************")
     println("")
-  } 
+  }
+
+  private def printHelpMessage() {
+    println("Usage:")
+    println("betex_feeder bfUser=? bfPassword=? bfProductId=? collectionInterval=? discoveryInterval=? startInMinutesFrom=? startInMinutesTo=? betexUrl=?\n")
+    println("bfUser/bfPassword/bfProductId - Betfair account that is used to collect market data using Betfair public API.")
+    println("collectionInterval - Market data collection interval in ms.")
+    println("discoveryInterval - New markets discovery interval in ms.")
+    println("startInMinutesFrom/startInMinutesTo - Market data is collected for markets with market time between startInMinutesFrom and startInMinutesTo.")
+    println("betexUrl - Betting Exchange Url that market data is feeded to.")
+    println("maxNumOfWinners - Collect market data for markets with number of winners less or equal to maxNumOfWinners. Default = Intifity.")
+  }
 }
---
+++ trunk/src/main/scala/dk/betex/feeder/MarketEventListener.scala
@@ -0,0 +1,28 @@
+package dk.betex.feeder
+
+import dk.bettingai.marketcollector.task.EventCollectorTask.EventListener
+import dk.bettingai.marketsimulator.marketevent._
+import dk.betex.api._
+
+/**
+ * Feeds markets events to a betting exchange.
+ *
+ * @author korzekwad
+ */
+case class MarketEventListener(betexAdaptor:IBetex) extends EventListener {
+
+  val userId = Integer.MAX_VALUE / 3
+  val marketEventProcessor = new MarketEventProcessorImpl(betexAdaptor)
+
+  var lastBetId = 1
+
+  def onEvents(marketId: Long, events: List[String]): Unit = {
+
+    def nextBetId(): Int = {
+      lastBetId += 1
+      lastBetId
+    }
+
+    events.foreach(e => marketEventProcessor.process(e, nextBetId, userId))
+  }
+}

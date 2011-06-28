840eb0d13016cfe4b5b868d76efbec08853774db
daniel.korzekwa <daniel.korzekwa@851a5dee-4224-aa8f-6f06-4bf9afbe9161>
2011-06-28 18:51:22 +0000

    Issue 2:	Remove old markets

--- trunk/src/main/scala/dk/betex/feeder/MarketEventListener.scala
+++ trunk/src/main/scala/dk/betex/feeder/MarketEventListener.scala
@@ -5,6 +5,8 @@ import dk.betex.eventcollector.eventprocessor._
 import dk.betex.api._
 import com.sun.jersey.api.client._
 import org.codehaus.jettison.json._
+import scala.collection.JavaConversions._
+import org.slf4j.LoggerFactory
 
 /**
  * Feeds markets events to a betting exchange.
@@ -13,7 +15,9 @@ import org.codehaus.jettison.json._
  */
 case class MarketEventListener(betexUrl: String) extends EventListener {
 
-  val userId = Integer.MAX_VALUE / 3
+  private val log = LoggerFactory.getLogger(getClass)
+
+  private val userId = Integer.MAX_VALUE / 3
 
   /**Client and resource are thread safe objects.*/
   private val client = Client.create()
@@ -31,5 +35,22 @@ case class MarketEventListener(betexUrl: String) extends EventListener {
 
     resource.path("/processBetexEvents").`type`("application/json").post(classOf[String], marketEventsData.toString)
 
+  }
+
+  override def onMarketDiscovery(marketIds: List[Long]) = {
+
+    val markets = resource.path("/getMarkets").get(classOf[String])
+    val marketsJSON = new JSONObject(markets)
+    val marketsJSONArray = marketsJSON.getJSONArray("markets")
+    val betexMarketIds = for (i <- 0 until marketsJSONArray.length) yield marketsJSONArray.getJSONObject(i).getLong("marketId")
+
+    /**Remove expired markets.*/
+    val marketsToBeRemoved = betexMarketIds.toList -- marketIds
+    for (marketId <- marketsToBeRemoved) {
+      val removeMarketStatus = resource.path("/removeMarket").queryParam("marketId", marketId.toString).get(classOf[String])
+      val removeMarketStatusJSON = new JSONObject(removeMarketStatus)
+      require(removeMarketStatusJSON.get("status") == "OK", "Removing market from betting exchange failed")
+      log.info("Market %s has been removed from Betex".format(marketId))
+    }
   }
 }
